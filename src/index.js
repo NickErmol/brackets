709f4ce8cbe658ecdc10876a8bd3ca13b7cc3d99
Nickerm <70840188+NickErmol@users.noreply.github.com>
2020-10-20 14:13:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairs = [];
+    bracketsConfig.forEach(item => pairs.push(item[0] + item[1]))
+
+    for (let i = 0; i < pairs.length; i++) {
+        if (str.includes(pairs[i])) {
+            str = str.replace(pairs[i], '')
+            i = - 1;
+        }
+    }
+    
+    return str === ''
+    
 }
